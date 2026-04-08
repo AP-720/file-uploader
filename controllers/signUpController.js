c18017d598456fc7aa688623e8072f319ebdcfd0
Andy Price <andyprice720@gmail.com>
2026-04-08 16:41:13 +0100

    feat(sign-up): add input validation

--- controllers/signUpController.js
+++ controllers/signUpController.js
@@ -1,24 +1,77 @@
 const { prisma } = require("../lib/prisma.js");
 const bcrypt = require("bcryptjs");
+const { body, validationResult, matchedData } = require("express-validator");
+
+const validateSignup = [
+	body("firstName")
+		.trim()
+		.notEmpty()
+		.withMessage(`First name is required`)
+		.isAlpha()
+		.withMessage(`First name must only contain letters.`),
+	body("lastName")
+		.trim()
+		.notEmpty()
+		.withMessage(`Last name is required`)
+		.isAlpha()
+		.withMessage(`Last name must only contain letters.`),
+	body("email")
+		.trim()
+		.notEmpty()
+		.withMessage(`Email is required.`)
+		.isEmail()
+		.withMessage(`Must be valid e-mail address.`),
+	body("password")
+		.trim()
+		.notEmpty()
+		.withMessage(`Password required.`)
+		.isStrongPassword({
+			minLength: 8,
+			minLowercase: 1,
+			minUppercase: 1,
+			minNumbers: 1,
+			minSymbols: 1,
+		})
+		.withMessage(
+			`Password must contain at least 8 characters. Including a minium of one lower case letter, one upper case letter, one number and one symbol.`,
+		),
+	body("confirmPassword")
+		.trim()
+		.notEmpty()
+		.withMessage(`Confirm password required.`)
+		.custom((value, { req }) => {
+			return value === req.body.password;
+		})
+		.withMessage("Passwords do not match. Re-enter and try again."),
+];
 
 async function getSignUp(req, res) {
 	res.render("signUp", { title: "Sign Up" });
 }
 
-async function postSignUp(req, res) {
-	console.log(req.body);
+const postSignup = [
+	validateSignup,
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res
+				.status(400)
+				.render("signUp", { title: "Sign Up", errors: errors.array() });
+		}
 
-	const { firstName, lastName, email, password } = req.body;
+		const { firstName, lastName, email, password } = matchedData(req);
 
-	try {
-		const hashedPassword = await bcrypt.hash(password, 10);
-		await prisma.user.create({
-			data: { firstName, lastName, email, password: hashedPassword },
-		});
-		res.redirect("/");
-	} catch (err) {
-		console.error(err);
-		res.status(500).send("Server error");
-	}
-}
-module.exports = { getSignUp, postSignUp };
+		try {
+			const hashedPassword = await bcrypt.hash(password, 10);
+			await prisma.user.create({
+				data: { firstName, lastName, email, password: hashedPassword },
+			});
+			res.redirect("/");
+		} catch (err) {
+			console.error(err);
+			res.status(500).send("Server error");
+		}
+	},
+];
+
+module.exports = { getSignUp, postSignup };
--- routes/signUp.js
+++ routes/signUp.js
@@ -4,6 +4,6 @@ const signUpController = require("../controllers/signUpController");
 const signUpRouter = Router();
 
 signUpRouter.get("/", signUpController.getSignUp);
-signUpRouter.post("/", signUpController.postSignUp);
+signUpRouter.post("/", signUpController.postSignup);
 
 module.exports = signUpRouter;
